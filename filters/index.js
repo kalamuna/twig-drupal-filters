3fed6648261c9aec0712c9d48164dff7dca7bdf1
Rob Loach <robloach@gmail.com>
2017-01-10 14:07:48 -0500

    Split uot clean_class and clean_id to own files

--- filters/index.js
+++ filters/index.js
@@ -1,16 +1,15 @@
 var trans = require('./trans');
-var slug = require('slug')
 
 module.exports = {
-  'link': require('./link'),
-  't': trans,
-  'trans': trans,
-  'placeholder': trans,
-  'without': trans,
-  'clean_class': slug,
-  'clean_id': slug,
-  'render': trans,
-  'format_date': trans,
-  'drupal_escape': require('./drupal_escape'),
-  'safe_join': require('./safe_join')
+  link: require('./link'),
+  t: trans,
+  trans: trans,
+  placeholder: trans,
+  without: trans,
+  clean_class: require('./clean_class'),
+  clean_id: require('./clean_id'),
+  render: trans,
+  format_date: trans,
+  drupal_escape: require('./drupal_escape'),
+  safe_join: require('./safe_join')
 };
--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-var assert = require('assert'),
-    twigFilters = require('..'),
-    Twig = require('twig'),
-    twig = Twig.twig;
+var assert = require('assert')
+var twigFilters = require('..')
+var Twig = require('twig')
+var twig = Twig.twig
 
 describe('twig-drupal', function(){
   // Add the Twig Filters to Twig.
@@ -9,18 +9,18 @@ describe('twig-drupal', function(){
 
   it('should use the link filter', function(done){
     var template = twig({
-      data: "{{ value|link('http://google.com') }}"
+      data: '{{ value|link("http://google.com") }}'
     });
-    var output = template.render({value: "Google"});
+    var output = template.render({value: 'Google'});
     assert.equal(output, '<a href="http://google.com">Google</a>');
     done();
   });
 
   it('should use the clean_class filter', function(done){
     var template = twig({
-      data: "{{ value|clean_class }}"
+      data: '{{ value|clean_class }}'
     });
-    var output = template.render({value: "Hello World!"});
+    var output = template.render({value: 'Hello World!'});
     assert.equal(output, 'Hello-World');
     done();
   });
