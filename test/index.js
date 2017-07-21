4d53d27ce0ab8b8963f1cff21cc86c66c7552d49
Derek DeRaps <hawkeye@tenderwolf.io>
2017-07-21 11:13:07 -0400

    Remove tests for the link filter

--- test/index.js
+++ test/index.js
@@ -8,15 +8,6 @@ describe('twig-drupal', function () {
   // Add the Twig Filters to Twig.
   twigFilters(twigPackage)
 
-  it('should use the link filter', function (done) {
-    var template = twig({
-      data: '{{ value|link("http://google.com") }}'
-    })
-    var output = template.render({value: 'Google'})
-    assert.equal(output, '<a href="http://google.com">Google</a>')
-    done()
-  })
-
   it('should use the clean_class filter', function (done) {
     var template = twig({
       data: '{{ value|clean_class }}'
