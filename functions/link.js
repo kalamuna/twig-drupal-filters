dcce601842b4a9adadc2a8f6e107f6fff1bb8ed2
Derek DeRaps <derekderaps@users.noreply.github.com>
2017-07-21 10:38:04 -0400

    Replace redundant code
    Reduces code duplication.

--- functions/link.js
+++ functions/link.js
@@ -4,17 +4,15 @@ module.exports = function (title, url, attributes) {
 
   // Loop through all the given attributes.
   for (let attribute in attributes) {
-    // If it's a string, just output it.
-    if (typeof attributes[attributes] === 'string') {
-      finalAttributes += ' ' + attribute + '="' + attributes[attribute] + '"'
+    let attributeValue = attributes[attribute];
 
-    // Otherwise, allow arrays to concat to strings.
-    } else if (attributes[attribute].join) {
-      finalAttributes += ' ' + attribute + '="' + attributes[attribute].join(' ') + '"'
+    // Support arrays in the attributes list (e.g., class).
+    if (attributeValue.isArray()) {
+      finalAttributes += ' ' + attribute + '="' + attributeValue.join(' ') + '"'
 
-    // Otherwise, let them figure it out.
+    // Otherwise, allow toString() to do its thing.
     } else {
-      finalAttributes += ' ' + attribute + '="' + attributes[attribute] + '"'
+      finalAttributes += ' ' + attribute + '="' + attributeValue + '"'
     }
   }
 
