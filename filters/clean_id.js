28b062d2e5dbc11871bf189b00aa2ec09f43fbdc
Wade Burelbach <fafnirical@users.noreply.github.com>
2019-10-10 21:43:52 -0700

    Port clean_id filter logic from Drupal

--- filters/clean_id.js
+++ filters/clean_id.js
@@ -1,1 +1,45 @@
-module.exports = require('./clean_class')
+/**
+ * Prepares a string for use as a valid HTML ID.
+ *
+ * @param {string} id
+ *   The ID to clean.
+ *
+ * @return {string}
+ *   The cleaned ID.
+ *
+ * @see \Drupal\Component\Utility\Html::getId()
+ */
+module.exports = function (id) {
+  var filter = {
+    ' ': '-',
+    _: '-',
+    '[': '-',
+    ']': ''
+  }
+
+  id = id.toLowerCase().replace(
+    new RegExp(
+      Object.keys(filter)
+        .map(function (value) {
+          return `(${value.replace(/[\\?*+|.^${}[\]()]/g, '\\$&')})`
+        })
+        .join('|'),
+      'g'
+    ),
+    function (substring) {
+      return filter[substring]
+    }
+  )
+
+  // As defined in http://www.w3.org/TR/html4/types.html#type-name, HTML IDs can
+  // only contain letters, digits ([0-9]), hyphens ("-"), underscores ("_"),
+  // colons (":"), and periods ("."). We strip out any character not in that
+  // list. Note that the CSS spec doesn't allow colons or periods in identifiers
+  // (http://www.w3.org/TR/CSS21/syndata.html#characters), so we strip those two
+  // characters as well.
+  id = id.replace(/[^A-Za-z0-9\-_]/g, '')
+
+  // Removing multiple consecutive hyphens.
+  id = id.replace(/-+/g, '-')
+  return id
+}
